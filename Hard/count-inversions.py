ffcd25560304c89b217b1d9c290eddac9c96275d
Nithinkumarva <152359686+Nithinkumarva@users.noreply.github.com>
2026-02-19 14:47:36 +0530

    Add solution for Count Inversions in Array

---
+++ Hard/count-inversions.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def countInversions(arr: List[int]) -> int:
+    # Your code here
+    pass
+
+# Read input
+arr = list(map(int, input().split()))
+print(countInversions(arr))
